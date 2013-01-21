9b3d1d766433332a9a712d4ecc2b8630bc1e19dc
Nikias Bassen <nikias@gmx.li>
2013-01-21 18:19:46 +0100

    updated OSX launcher script for Mountain Lion

--- resources/osx/Absinthe-launcher.sh
+++ resources/osx/Absinthe-launcher.sh
@@ -2,13 +2,14 @@
 C=`dirname "${0}"`
 
 case `sysctl -n kern.osversion |sed 's/[A-Z]/:/g' |cut -d ":" -f 1` in
-11|10)
+12|11|10)
 	exec "$C/Absinthe-10.6"
 	;;
 9)
 	exec "$C/Absinthe-10.5"
 	;;
 *)
+	osascript -e 'tell app "Finder" to display alert "Not supported" message "Sorry, Absinthe is not supported on this version of Mac OS X."'
 	exit 1
 	;;
 esac
